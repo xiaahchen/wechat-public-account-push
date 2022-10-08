4c6cc647b9ad86c88579a37ab62bb017389febaa
xiaahchen <85912086+xiaahchen@users.noreply.github.com>
2022-10-08 18:39:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oJ2Op6N2ZfhzTtBUcxOXL9wyKLGY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'In_sEXMw1pkq1IZW0eBUufjsxzZiv_EQa8CMNm1fktw',
+      useTemplateId: 'ZumehaHm9ReQkBOon7SCtZNoKaVNzxmfzl6g-HiLzvk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-21',
       festivals: [
@@ -50,7 +50,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'In_sEXMw1pkq1IZW0eBUufjsxzZiv_EQa8CMNm1fktw',
+  CALLBACK_TEMPLATE_ID: 'ZumehaHm9ReQkBOon7SCtZNoKaVNzxmfzl6g-HiLzvk',
 
   CALLBACK_USERS: [
     {
