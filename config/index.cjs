2269682a43fb0d523a857e8ca9306658db77b4ea
xiaahchen <85912086+xiaahchen@users.noreply.github.com>
2022-10-08 17:20:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-05-21 },
+        { keyword: 'love_day', date: '2022-05-21' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-04-26' },
       ],
